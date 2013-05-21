8f2d5ef965067e9bf49c9b7dc8acc628e05a59f9
Vineet Sethi <developer.vineet@gmail.com>
2013-05-21 23:02:05 +0530

    Home page + Header Menu (after login)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,17 +5,4 @@ class ApplicationController < ActionController::Base
     stored_location_for(resource_or_scope) || account_index_path
   end
   
-#  def present_user
-#    if user_signed_in? 
-#      @present_user = current_user
-#    elsif buyer_signed_in?
-#      @present_user = current_buyer
-#    elsif service_provider_signed_in?
-#      @present_user = current_
-#    else  
-#    
-#    end
-#  end
-#  helper_method :current_user
-
 end
